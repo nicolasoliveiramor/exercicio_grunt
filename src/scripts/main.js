e8740f1b6ad5f3aa7db0e55e07372b268eb95e8c
Nicolas Oliveira Mor <nicolasoliveiramor05@gmail.com>
2024-08-24 20:22:26 -0300

    fixed: dizima periodica

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,32 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.addEventListener('submit', function(e){
-        e.preventDefault()
-            const num1 = document.getElementById('num-1')
-            const num2 = document.getElementById('num-2')
-            const resultValor = document.getElementById('valor-resultado')
-        
-            document.getElementById('soma').addEventListener('click', function() {
-                const soma = parseFloat(num1.value) + parseFloat(num2.value)
-                resultValor.textContent = soma
-            })
-        
-            document.getElementById('sub').addEventListener('click', function() {
-                const sub = parseFloat(num1.value) - parseFloat(num2.value)
-                resultValor.textContent = sub
-            })
-        
-            document.getElementById('multi').addEventListener('click', function() {
-                const multi = parseFloat(num1.value) * parseFloat(num2.value)
-                resultValor.textContent = multi
-            })
-        
-            document.getElementById('div').addEventListener('click', function() {
-                if (parseFloat(num2.value) === 0) {
-                    resultValor.textContent = '0'
-            } else {
-                const div = parseFloat(num1.value) / parseFloat(num2.value)
-                resultValor.textContent = div
-            }
-        })
+    const num1 = document.getElementById('num-1')
+    const num2 = document.getElementById('num-2')
+    const resultValor = document.getElementById('valor-resultado')
+
+    document.getElementById('soma').addEventListener('click', function() {
+        const soma = parseInt(num1.value) + parseInt(num2.value)
+        resultValor.textContent = soma
+    })
+
+    document.getElementById('sub').addEventListener('click', function() {
+        const sub = parseInt(num1.value) - parseInt(num2.value);
+        resultValor.textContent = sub;
+    });
+
+    document.getElementById('multi').addEventListener('click', function() {
+        const multi = parseInt(num1.value) * parseInt(num2.value);
+        resultValor.textContent = multi;
+    });
+
+    document.getElementById('div').addEventListener('click', function() {
+        if (parseFloat(num2.value) === 0) {
+            resultValor.textContent = 'Erro: Divisão por zero'
+        } else {
+            const div = parseFloat(num1.value) / parseFloat(num2.value)
+            resultValor.textContent = div.toFixed(2)
+        }
     })
 })
